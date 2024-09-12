6cbff5f394a7fbafb2044bc7b5b87528014c615d
Jovin Liu <liujovin@gmail.com>
2024-09-13 02:12:31 +1000

    2024093

--- src/UI/TopicListItem.jsx
+++ src/UI/TopicListItem.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 import styled from "styled-components";
-import TitleListItem from "./titleListItem";
+import TitleListItem from "./TitleListItem";
 
 /* eslint-disable react/prop-types */
 const Li = styled.li`
